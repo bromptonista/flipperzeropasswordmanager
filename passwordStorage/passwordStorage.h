374aac46e0abb5895b11c4b53453bb2f00e42278
bromptonista <sarandos@gmail.com>
2025-05-02 16:31:36 -0700

    Update passwordStorage.h
    Added comments

--- passwordStorage/passwordStorage.h
+++ passwordStorage/passwordStorage.h
@@ -6,6 +6,37 @@
 #include <stdlib.h>
 #include "../main.h"
 
+/**
+ * @brief Reads credentials from a CSV-formatted file.
+ *
+ * Each line must be in the format: service,username,password
+ * 
+ * @param filename Path to the password file.
+ * @param credentials Array to store the loaded credentials.
+ * @return Number of credentials successfully read.
+ */
 size_t read_passwords_from_file(const char* filename, Credential* credentials);
+
+/**
+ * @brief Appends a new credential entry to the password file.
+ * 
+ * Writes the entry as a CSV line: service,username,password
+ *
+ * @param filename Path to the password file.
+ * @param service Name of the service (e.g., "Gmail").
+ * @param username Associated username.
+ * @param password Associated password.
+ * @return true if successfully written, false otherwise.
+ */
 bool write_password_to_file(const char* filename, const char* service, const char* username, const char* password);
+
+/**
+ * @brief Deletes a specific line from a password file.
+ * 
+ * Line index is zero-based (i.e., line 0 is the first line).
+ *
+ * @param path Path to the password file.
+ * @param line_to_delete Index of the line to remove.
+ * @return true on successful deletion and file update, false otherwise.
+ */
 bool delete_line_from_file(const char* path, size_t line_to_delete);
